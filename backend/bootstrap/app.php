63804f819db810fbc21b38ff88692c1281b2f1ad
Jude mpoyo <mpoyojude0@gmail.com>
2025-03-13 16:22:32 +0200

    Configure middleware for API authentication and enhance Sanctum driver setup

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,6 +14,15 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->statefulApi();
+    $middleware->alias([
+        'auth' => \App\Http\Middleware\Authenticate::class,
+    ]);
+    
+    $middleware->group('api', [
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
